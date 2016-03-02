305102e83b7c11a48639af5dde29deeced362594
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-03-02 02:39:08 -0500

    skel

---
+++ Turtar/src/systems/scripted-movement.lisp
@@ -0,0 +1,25 @@
+(defpackage scripted-movement
+  (:use :cl :oliphaunt :turtar)
+  (:export scripted-movement))
+(in-package scripted-movement)
+
+;;; scripted-movement
+;;; Part of Turtar
+;;; Copyright © 2016, Bruce-Robert Fenn Pocock
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU Affero General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Affero General Public License for more details.
+
+;;; You should have received a copy of the GNU Affero General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(defclass scripted-movement ()
+  ())
+
