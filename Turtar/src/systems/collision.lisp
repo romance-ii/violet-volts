e41385a490b670fd125331165f29e25fbe3904da
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-03-15 19:02:56 -0400

    ☠

---
+++ Turtar/src/systems/collision.lisp
@@ -0,0 +1,10 @@
+;;; -*- lisp -*-
+(defpackage turtar/collision
+  (:use :cl :oliphaunt :turtar/thing :turtar/geometry))
+
+(in-package :turtar/collision)
+
+;;; Copyright  © 2016, Bruce-Robert Fenn Pocock <brpocock@Star-Hope.org>
+
+;;; See doc/collision.texi
+
