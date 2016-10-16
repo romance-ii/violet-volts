c56c3687a4cc560ca2708ff53eb95d4785459a37
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-10-16 00:18:57 -0400

    toots1015

--- src/bootstrap-tootstest.lisp
+++ src/bootstrap-tootstest.lisp
@@ -2,16 +2,19 @@
 (in-package :jscl)
 
 (defun bootstrap-mesh ()
-  (bootstrap)
+  (bootstrap-core)
   (let ((*features* (list* :jscl :jscl-xc *features*))
         (*package* (find-package "JSCL"))
         (*default-pathname-defaults* *base-directory*)) 
     (compile-application
      (cons
       (source-pathname "package" :directory '(:relative :up :up :up "src" "mesh")
-                                 :type "lisp")
-      (directory (source-pathname "*" :directory '(:relative :up :up :up "src" "mesh")
-                                      :type "lisp")))
+                       :type "lisp")
+      (remove-if
+       (lambda (path)
+         (equal (pathname-name path) "package"))
+       (directory (source-pathname "*" :directory '(:relative :up :up :up "src" "mesh")
+                                   :type "lisp"))))
      (source-pathname "mesh" :directory '(:relative :up :up :up "js")
-                             :type "js"))))
+                      :type "js"))))
 
