5c142718be94de6fcbf1185a580a13fdc3e2e88d
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-10-17 01:24:51 -0400

    work-around not needed on Cherryville

--- src/setup.lisp
+++ src/setup.lisp
@@ -1,3 +1,4 @@
-(load (merge-pathnames #p".sbclrc" (user-homedir-pathname)))
-(load #p"SYS:CONTRIB;**;sb-introspect.fasl.NEWEST")
-(load #p"SYS:CONTRIB;**;sb-rotate-byte.fasl.NEWEST")
+(unless (equal (machine-instance) "cherryville")
+  (load (merge-pathnames #p".sbclrc" (user-homedir-pathname)))
+  (load #p"SYS:CONTRIB;**;sb-introspect.fasl.NEWEST")
+  (load #p"SYS:CONTRIB;**;sb-rotate-byte.fasl.NEWEST"))
