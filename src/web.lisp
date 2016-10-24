fa8b7823262dfdace2a2f3597c8285a805dab2f0
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-10-24 19:29:44 -0400

    Google sign-in passing

--- src/web.lisp
+++ src/web.lisp
@@ -55,12 +55,81 @@
                                  :directory '(:relative "js")
                                  :name name :type "js")
                                 *static-directory*)))
-
+
+;;; Login
 
 (defroute "/tootstest/login" ()
   (render #P"login.html"))
 
-;; News
+(defvar *google-public-key*)
+
+(defun fetch-json (uri)
+  (yason:parse
+   (map 'string #'code-char
+        (drakma:http-request uri :method :get :accept "application/json;charset=utf-8"))
+   :object-as :plist
+   :object-key-fn (compose #'make-keyword #'string-upcase)
+   :json-arrays-as-vectors t))
+
+(defun get-google-public-key ()
+  (or *google-public-key*
+      (setf *google-public-key*
+            (fetch-json (getf (fetch-json "https://accounts.google.com/.well-known/openid-configuration")
+                              :jwks_uri)))))
+
+(defun check-jwk (json-web-key header body signature)
+  "TODO: Split  id-token into three  parts on #\.  and pass in  with the
+public         key        from         Google.        Similar         to
+http://ncona.com/2015/02/consuming-a-google-id-token-from-a-server/ "
+  :fixme)
+
+(define-constant +google-client-ids+
+    '("1052481910502-4tkek5mbv4jf9o16oaclg1v3ugrruilg.apps.googleusercontent.com"
+      "1052481910502-sumg845at7g627qdvhhhambm7c7do8e4.apps.googleusercontent.com")
+  :test #'equalp)
+
+(defun user<-google-token (id-token)
+  (multiple-value-bind
+        (response-binary status response-headers response-uri
+                         stream stream-closed-p status-reason)
+      (drakma:http-request "https://www.googleapis.com/oauth2/v3/tokeninfo"
+                           :parameters (list (cons "id_token" id-token))
+                           :method :get
+                           :accept "application/json;charset=utf-8")
+    (declare (ignore stream stream-closed-p))
+    (assert (<= 200 status 299) ()
+            "While validating Google sign-in token, error ~D: ~A returned by ~A"
+            status status-reason response-uri)
+    (assert (string= "https://www.googleapis.com/"
+                     response-uri :end2 27) ()
+                     "While validating Google sign-in token, response returned by ~A, whose authority I do not recognize"
+                     response-uri)
+    (let* ((response (yason:parse (map 'string #'code-char response-binary)
+                                  :object-as :plist
+                                  :object-key-fn (compose #'make-keyword #'string-upcase)
+                                  :json-arrays-as-vectors t)))
+      (assert (find (getf response :iss) '("accounts.google.com" "https://accounts.google.com")
+                    :test #'string-equal) ()
+                    "While validating Google sign-in token, ISS field returned was ~A, whose authority I do not recognize"
+                    (getf response :iss))
+      (assert (< (parse-integer (getf response :exp))
+                 (local-time:timestamp-to-unix (local-time:now))) ()
+                 "The Google sign-in permission has already expired.")
+      (getf response :sub))))
+
+(defroute ("/tootstest/login/registrars/:registrar" :method :post)
+    (&key registrar id-token)
+  (handler-case
+      (let ((user (ecase (make-keyword (string-upcase registrar))
+                    (:google (user<-google-token id-token))
+                    (:facebook (error "Facebook sign-in is not yet supported")))))
+        (when user
+          (render-player-details :google user))) 
+    (error (c)
+      (render-json (:error (princ-to-string c))))))
+
+
+;;; News
 
 (defvar *tootsbook-cache* nil)
 (defvar *tootsbook-fetched* 0)
@@ -120,7 +189,7 @@
 (defroute "/tootstest/zomg" ()
   (assert (string-equal +application/json+ (request-content-type *request*)
                         :end2 #.(length +application/json+))
-          () "Zombies should always use JSON, not ~a" (request-content-type *request*)) 
+          () "Zombies should always use JSON, not ~a" (request-content-type *request*))
   (let ((report (let ((yason:*parse-object-as* :plist)
                       (yason:*PARSE-json-arrays-as-vectors* t))
                   (yason:parse (request-raw-body *request*)))))
