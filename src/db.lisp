ad573c5bae734b5415b54b62318b060553d8f31d
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-10-24 15:53:33 -0400

    add DB secrets and JOURNAL

--- src/db.lisp
+++ src/db.lisp
@@ -8,11 +8,20 @@
                 :connect-cached)
   (:export :connection-settings
            :db
-           :with-connection))
+           :with-connection
+           :journal))
 (in-package :tootstest.db)
 
+(defvar *db-secrets*)
+
 (defun connection-settings (&optional (db :maindb))
-  (cdr (assoc db (config :databases))))
+  (unless *db-secrets*
+    (let ((secret-file))
+      (if (probe-file secret-file)
+          (load secret-file)
+          (warn "db-secrets not set; database access will probably fail; tried ~s" secret-file))))
+  (or (ignore-errors (cdr (assoc db *db-secrets*)))
+      (cdr (assoc db (config :databases)))))
 
 (defun db (&optional (db :maindb))
   (apply #'connect-cached (connection-settings db)))
@@ -20,3 +29,41 @@
 (defmacro with-connection (conn &body body)
   `(let ((*connection* ,conn))
      ,@body))
+
+
+
+(defun journal-node (node &optional prior)
+  (etypecase node
+    (list (datafly:execute (sxql:insert-into :journal-nodes
+                                             :event-time *now*
+                                             :node-type "list"
+                                             :key nil
+                                             :value nil
+                                             :prior prior))
+          (let ((parent-id (datafly:retrieve-one (sxql:select (:last-insert-id)))))
+            loop for child in node
+            with prior-child
+            do (journal-node child prior-child)
+            do (setq prior-child child)))
+    (hash-table (datafly:execute (sxql:insert-into :journal-nodes
+                                                   :event-time *now*
+                                                   :node-type "list"
+                                                   :key nil
+                                                   :value nil
+                                                   :prior prior))
+                (let ((parent-id (datafly:retrieve-one (sxql:select (:last-insert-id)))))
+                  loop for child in node
+                  with prior-child
+                  do (journal-node child prior-child)
+                  do (setq prior-child child)))
+    (atom (datafly:execute (sxql:insert-into :journal-nodes
+                                             :event-time *now*
+                                             :node-type (simple-type-of node)
+                                             :key nil
+                                             :value nil
+                                             :prior prior)))))
+
+(defun journal (&rest tree)
+  (with-connection (:journal)
+    (let ((*now* (local-time:timestamp-to-universal (local-time:now))))
+      (journal-node tree))))
