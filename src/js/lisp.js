d6d0b96c1f31cc68e7ce0093beab06127131dfa2
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-10-24 03:41:13 -0400

    ZOMG Gazonks hook

--- src/js/lisp.js
+++ src/js/lisp.js
@@ -23,3 +23,57 @@ window.curry_lisp = function (symbol) {
     }
 }
 
+(function(){
+    var nextMethod = window.onerror;
+    window.onerror = function (message, source, line, column, error) {
+        console.error("Unhandled error: " + message +
+                      "\n\tSource: " + (source || '?') +
+                      "\n\tLine: " + (line || '?') + "\tColumn: " + (column || '?'));
+        if (error) { console.error(error) }
+        console.log("Reporting home…");
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', "/tootstest/zomg", true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        xhr.send(JSON.stringify({
+            "condition": "window.onerror",
+            "catch": "gazonga",
+            "product": "tootstest/0.2",
+            "navigator": {	"appCodeName": appName.appCodeName,
+                              "appName": navigator.appName,
+                              "appVersion": navigator.appVersion,
+                              "buildID": navigator.buildID,
+                              "connection": navigator.connection &&
+                              { "type": navigator.connection.type,
+                                "downlinkMax": navigator.connection.downlinkMax },
+                              "hardwareConcurrency": navigator.hardwareConcurrency,
+                              "javaEnabled": navigator.javaEnabled,
+                              "language": navigator.language,
+                              "languages": navigator.languages,
+                              "mimeTypes": navigator.mimeTypes,
+                              "onLine": navigator.onLine,
+                              "oscpu": navigator.oscpu,
+                              "platform": navigator.platform,
+                              "product": navigator.product,
+                              "userAgent": navigator.userAgent,
+                              "cookieEnabled": navigator.cookieEnabled,
+                              "doNotTrack": navigator.doNotTrack,
+                              "BrowserID": navigator.id,
+                              "productSub": navigator.productSub,
+                              "vendor": navigator.vendor,
+                              "vendorSub": navigator.vendorSub,
+                              "standalone", navigator.standalone },
+            "message": message,
+            "source": source,
+            "line": line,
+            "column": column,
+            "error": {	"name": (error && error.name),
+                              "message": (error && error.message),
+                              "fileName": (error && error.fileName),
+                              "lineNumber": (error && error.lineNumber),
+                              "columnNumber": (error && error.columnNumber),
+                              "stack": (error && error.stack) }}));
+        if (nextMethod) {
+            nextMethod(message, source, line, column, error);
+        }
+    }
+})()
