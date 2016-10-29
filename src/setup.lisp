40428967eae3679e72b5e10dc8fded1e224143ee
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-10-29 00:19:24 -0400

    Specialize image patching on package presence.
    The former version just checked the hostname, but it was not
    really just that one host that “worked right,” so this version
    checks for the presence of the packages and then tries to load
    the FASLs if they are not defined.

--- src/setup.lisp
+++ src/setup.lisp
@@ -1,4 +1,6 @@
-(unless (equal (machine-instance) "cherryville")
-  (load (merge-pathnames #p".sbclrc" (user-homedir-pathname)))
-  (load #p"SYS:CONTRIB;**;sb-introspect.fasl.NEWEST")
+(load (merge-pathnames #p".sbclrc" (user-homedir-pathname)))
+(unless (find-package :sb-introspect)
+  (load #p"SYS:CONTRIB;**;sb-introspect.fasl.NEWEST"))
+(unless (find-package :sb-rotate-byte)
   (load #p"SYS:CONTRIB;**;sb-rotate-byte.fasl.NEWEST"))
+
