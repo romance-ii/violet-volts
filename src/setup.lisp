20664ec77e8dc31d834dcbca1efdc6e09bca43a0
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-10-30 17:26:05 -0400

    Try harder before calling back on loading FASL manudally

--- src/setup.lisp
+++ src/setup.lisp
@@ -1,6 +1,8 @@
 (load (merge-pathnames #p".sbclrc" (user-homedir-pathname)))
+(ignore-errors (require 'sb-introspect))
 (unless (find-package :sb-introspect)
   (load #p"SYS:CONTRIB;**;sb-introspect.fasl.NEWEST"))
+(ignore-errors (require 'sb-rotate-byte))
 (unless (find-package :sb-rotate-byte)
   (load #p"SYS:CONTRIB;**;sb-rotate-byte.fasl.NEWEST"))
 
