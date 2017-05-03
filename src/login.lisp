2cdaad4eea5dc8bd6c97bb3294e12edb933efe28
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2017-05-03 19:05:24 -0400

    facelifts to login page

--- src/login.lisp
+++ src/login.lisp
@@ -2,7 +2,7 @@
 (in-package :tootstest.web)
 
 (defroute route-/login "/login" ()
-          "The  login  page   itself  is  hosted  here   to  ensure  same-domain
+  "The  login  page   itself  is  hosted  here   to  ensure  same-domain
 interaction with the API calls. It's mostly a static page, but we render
 it through the template engine.
 
@@ -10,8 +10,9 @@ There is a  lot of “stuff” happening  on this page —  optimizing it will
 probably be a Godsend at some point. XXX
 
 This page should have some more precise headers to assist in caching."
-          (render #P"login.html"
-                  (list :server-url
-                        (format nil "//~(~a~):~d"
-                                (request-server-name *request*)
-                                (request-server-port *request*)))))
+  (render #P"login.html"
+          (list :server-url
+                (format nil "//~(~a~):~d"
+                        (request-server-name *request*)
+                        (request-server-port *request*))
+                :debug t)))
